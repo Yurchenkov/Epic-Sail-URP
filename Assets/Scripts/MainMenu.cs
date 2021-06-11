610b006ca46ce14aad492301c82f1efe7a74ab37
Yurchenkov <youonlyplaytwice@gmail.com>
2021-06-11 20:21:34 +0300

    Fixes UI scale for mobile platform

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,10 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    private void Start() {
-        Cursor.lockState = CursorLockMode.Confined;
-    }
-
     public void StartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }

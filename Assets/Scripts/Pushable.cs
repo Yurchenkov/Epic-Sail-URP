4e68ea323a959ab8464f67a8c0cf120e78df6b9a
MiNiGor <Mi.Ni.Gor@yandex.ru>
2021-06-24 00:01:02 +0300

    Fixed codestyle

--- Assets/Scripts/Pushable.cs
+++ Assets/Scripts/Pushable.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
 
-public class Pushable : MonoBehaviour
-{
+public class Pushable : MonoBehaviour {
     private Pointer _pointer;
 
     private bool _isToched = false;
     private Rigidbody _rigidbody;
     private Vector3 _startTrailPosition;
 
-    private void Awake()
-    {
+    private void Awake() {
         _pointer = GameObject.FindGameObjectWithTag(GameManager.TAG_POINTER).GetComponent<Pointer>();
         _rigidbody = GetComponent<Rigidbody>();
     }
@@ -22,25 +20,21 @@ public class Pushable : MonoBehaviour
             _isToched = false;
         }
     }
-    private Vector3 GetForceVector(Vector3 target)
-    {
+    private Vector3 GetForceVector(Vector3 target) {
         Vector3 forceVector = GetDirection(target).normalized;
         forceVector.y = 1f;
         return forceVector * GetDistance(target);
     }
-    private Vector3 GetMotionTarget()
-    {
+    private Vector3 GetMotionTarget() {
         Vector3 target = _pointer.lastTrailPointerPosition;
         return target;
     }
 
-    private float GetDistance(Vector3 target)
-    {
+    private float GetDistance(Vector3 target) {
         return GetDirection(target).magnitude;
     }
 
-    private Vector3 GetDirection(Vector3 target)
-    {
+    private Vector3 GetDirection(Vector3 target) {
         return target - transform.position;
     }
 

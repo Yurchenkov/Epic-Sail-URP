eb189cee0a84eed687985247b36ecdb4fb1d8aa2
Yurchenkov <youonlyplaytwice@gmail.com>
2021-06-23 20:20:19 +0300

    Adds animating movement tutorial

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour {
+
+    public GameObject tutorialCanvas;
+    public Animator handCursorAnimator;
+
+    private GameManager _gameManager;
+    private string _tutorialType;
+
+    private void Awake() {
+        _gameManager = GameObject.FindGameObjectWithTag(GameManager.TAG_GAME_MANAGER).GetComponent<GameManager>();
+        _tutorialType = gameObject.tag;
+    }
+
+    private void Update() {
+        CloseTutorial();
+    }
+
+    private void CloseTutorial() {
+        if (tutorialCanvas.activeSelf && Input.GetMouseButtonDown(0)) {
+            tutorialCanvas.SetActive(false);
+            GameManager.CompleteTutorial(_tutorialType);
+            ResumeGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(GameManager.TAG_PLAYER) && !GameManager.IsTutorialComplete(_tutorialType))
+            ShowTutorial();
+    }
+
+    public void ShowTutorial() {
+        _gameManager.Pause();
+        StartTutorialAnimation();
+    }
+
+    private void ResumeGame() {
+        _gameManager.Resume();
+    }
+
+    private void StartTutorialAnimation() {
+        tutorialCanvas.SetActive(true);
+        handCursorAnimator.SetTrigger(_tutorialType);
+    }
+}

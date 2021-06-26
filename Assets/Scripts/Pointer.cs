83adbe582f57ee29dfe8d731548c47ddcca9c39f
MiNiGor <Mi.Ni.Gor@yandex.ru>
2021-06-26 13:47:12 +0300

    Сhanges the input verification method
    Input verification was released in private method.

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -11,6 +11,10 @@ public class Pointer : MonoBehaviour {
     private bool _isPushSomething = false;
     private GameObject _pushedObject;
 
+    private Vector3 _iputPosition;
+    private bool _getTouch;
+    private bool _getTouchUp;
+
     private void Awake() {
         _gameManager = GameObject.FindGameObjectWithTag(GameManager.TAG_GAME_MANAGER).GetComponent<GameManager>();
         _trailRenderer = GetComponent<TrailRenderer>();
@@ -19,28 +23,30 @@ public class Pointer : MonoBehaviour {
     }
 
     private void Update() {
+        GameManager.CompleteTutorial("Movement");
         if (_gameManager.isGamePaused || !GameManager.IsTutorialComplete(GameManager.TUTORIAL_TYPE_MOVEMENT))
             return;
 
+        CheckInput();
         RenderTrail();
         Move();
         FillLastTrailPointerPosition();
     }
 
     private void RenderTrail() {
-        bool mouseInput = Input.GetMouseButton(0);
+        bool mouseInput = _getTouch;
         _trailRenderer.enabled = mouseInput;
         _pointerCollider.enabled = mouseInput;
     }
 
     private void Move() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(_iputPosition);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, _layerMask))
             transform.position = raycastHit.point;
     }
 
     private void FillLastTrailPointerPosition() {
-        if (Input.GetMouseButtonUp(0) && _isPushSomething) {
+        if (_getTouchUp && _isPushSomething) {
             lastTrailPointerPosition = transform.position;
 
             Pushable pushableComponent = _pushedObject.GetComponent<Pushable>();
@@ -55,6 +61,25 @@ public class Pointer : MonoBehaviour {
 
             _isPushSomething = false;
             _pushedObject = null;
+        }
+    }
+
+    private void CheckInput() {
+        _getTouchUp = false;
+        if (Input.touchCount>0) {
+            _iputPosition = Input.GetTouch(0).position;
+            _getTouch = true;
+            if (Input.GetTouch(0).phase == TouchPhase.Ended) {
+                _getTouchUp = true;
+            }
+        } else if (Input.GetMouseButton(0)) {
+            _iputPosition = Input.mousePosition;
+            _getTouch = true;
+        } else {
+            _getTouch = false;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            _getTouchUp = true;
         }
     }
 

33edde04d92fa5a7bc097231936d8bd212151590
Pavel Yurchenkov <youonlyplaytwice@gmail.com>
2021-06-28 20:00:55 +0300

    Fixes fatal error

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -13,11 +13,11 @@ public class LevelController : MonoBehaviour {
     private Transform _transform;
 
     private void Awake() {
-        GameManager.instance.currentLevelType = levelType.ToString();
         _transform = transform;
     }
 
     void Update() {
+        GameManager.instance.currentLevelType = levelType.ToString();
         if (GameManager.instance.currentLevelType == Constants.LEVEL_TYPE_LINEAR)
             _transform.Translate(-Vector3.right * speed * Time.deltaTime);
     }
